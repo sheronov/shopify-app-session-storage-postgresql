5530f642c133f656b2c413e01c15ee524dde4217
sheronov <e.sheronov@outlook.com>
2024-03-25 00:21:02 -0600

    respect SSL DB URL param

--- src/postgres-connection.ts
+++ src/postgres-connection.ts
@@ -62,6 +62,11 @@ export class PostgresConnection implements RdbmsConnection {
   public getDatabase(): string | undefined {
     return decodeURIComponent(this.dbUrl.pathname.slice(1));
   }
+  
+  public getSSL(): boolean | pg.ClientConfig['ssl'] | undefined {
+    const ssl = this.dbUrl.searchParams.get('ssl');
+    return ssl ? JSON.parse(ssl) : undefined;
+  }
 
   async hasTable(tablename: string): Promise<boolean> {
     await this.ready;
@@ -89,6 +94,7 @@ export class PostgresConnection implements RdbmsConnection {
       password: decodeURIComponent(this.dbUrl.password),
       database: this.getDatabase(),
       port: Number(this.dbUrl.port),
+      ssl: this.getSSL(),
     });
   }
 }
